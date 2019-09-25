409d6134f7f91b8d660bd8e068daba4fe61aad5d
Matt Bear <hi@matt.am>
2019-09-25 22:54:44 +1000

    install mongoose

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const taskpull = require("./taskpull");
 
-app.get('/ping', function (req, res) {
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/ping", function(req, res) {
   console.log("pong");
- return res.send('pong');
+  return res.send("pong");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
