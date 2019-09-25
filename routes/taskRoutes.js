a772bacb5c19f0b7916aac46e8264ca44bbfe159
Matt Bear <hi@matt.am>
2019-09-26 01:06:47 +1000

    add endpoints to view all (non-rejected) tasks, and change the status of tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,4 +2,23 @@ const mongoose = require("mongoose");
 
 const Task = mongoose.model("task");
 
-module.exports = app => {};
+module.exports = app => {
+  app.get("/api/tasks", (req, res) => {
+    Task.find({}).then(data => {
+      let results = [];
+      data.forEach(r => {
+        if (r.myStatus !== "reject") {
+          results.push(r);
+        }
+      });
+      res.status(200).send(results);
+    });
+  });
+  app.get("/api/tasks", (req, res) => {
+    const { id, myStatus } = req.body;
+    const task = { id, myStatus };
+    Task.updateOne({ id }, task, { upsert: true }).then(data => {
+      res.status(200).send(data);
+    });
+  });
+};
